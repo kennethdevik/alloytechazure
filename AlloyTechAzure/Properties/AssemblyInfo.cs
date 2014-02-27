5f48179f7136971c72b09a7f5ffe8909fa85a2ea
Kenneth Devik <kenneth@devik.net>
2014-02-27 22:25:38 +0100

    Bugfix?

--- AlloyTechAzure/Properties/AssemblyInfo.cs
+++ AlloyTechAzure/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AlloyTechAzure")]
+[assembly: AssemblyTitle("Alloy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlloyTechAzure")]
+[assembly: AssemblyProduct("Alloy")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
